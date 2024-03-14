c9154c411a8115d069ac26cf996dd9ccaa27781c
erasylmurat <157134780+erasylmurat@users.noreply.github.com>
2024-03-14 17:20:34 +0600

    lab6

---
+++ Built in functions.py
@@ -0,0 +1,50 @@
+#Exercise 1
+array = [int(x) for x in input().split()]
+def multiply(array):
+    total = 1
+    for x in array:
+        total *= x
+    return total
+print(multiply(array))
+
+#Exercise 2
+word = input()
+def up_low(word):
+    up = 0
+    low = 0
+    for x in word:
+        if x >= 'A' and x <= 'Z':
+            up += 1
+        if x >= 'a' and x <= 'z':
+            low += 1
+
+    print("Upper case letters:", up)
+    print("Lower case letters:", low)
+up_low(word)
+
+#Exercise 3
+word = input()
+def isPalindrome(word):
+    word = word.lower().replace(" ", "") #to convert chars to lower and remove whitespace 
+    return word==word[::-1]
+if(isPalindrome(word)==1):
+    print("Palindrome")
+else:
+    print("Not palindrome")
+
+#Exercise 4
+from time import sleep
+import math
+def delay(function, miliesec, *arguments):
+  sleep(miliesec / 1000)
+  return function(*arguments)
+print("Square root after specific miliseconds:")
+miliesec = int(input()) 
+arguments = int(input())
+print(delay(lambda x: math.sqrt(x), miliesec, arguments))
+
+#Exercise 5
+x = (True, True, False)
+y = (True, True, True)
+print(all(x))
+print(all(y))
